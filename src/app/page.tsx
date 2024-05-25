a433cbe7585f46fde50a82c7c11ada6f3b6c2f10
patrick-castro <castropatrickantonio@gmail.com>
2024-05-25 12:30:29 +0800

    Reload data after submitting form

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,6 +2,7 @@ import { Button } from '@/components/ui/button';
 import { Input } from '@/components/ui/input';
 import { database } from '@/db/database';
 import { bids as bidsSchema } from '@/db/schema';
+import { revalidatePath } from 'next/cache';
 
 export default async function Home() {
   const bids = await database.query.bids.findMany();
@@ -9,6 +10,7 @@ export default async function Home() {
   const handleSubmitForm = async (formData: FormData) => {
     'use server';
     await database.insert(bidsSchema).values({});
+    revalidatePath('/');
   };
 
   return (
